ebf29c481a513f2bb8aa513b4874d5cd5b1f36a1
John Aldrich Bernardo <bjohnaldrich@gmail.com>
2017-04-08 06:54:46 +0000

    Welcome page

--- lmvc/app/view/hello.php
+++ lmvc/app/view/hello.php
@@ -2,9 +2,37 @@
 <html>
     <head>
         <title>Hello World!</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
     <body>
-        <h1>Welcome to Lollipop-MVC</h1>
-        This page was rendered in <?= App::getBenchmark()->time_elapsed; ?>
+        <article>  
+            <pre class="candybox2">
+        OOOOOOOOOO
+      OO          OO
+     O OOOOOOOOOOOOOO
+    O OO          OOOO
+   O OO OOOOOOOOOOOOOOO
+  0 00 00000      00000
+  O OO OOOOO OOOO OOOOO
+   O OO OOOO  OOOO OOOO
+    O OO OOOOOOOO OOOO
+     OOOO        OOOO
+       OOOOOOOOOOOOO
+          OOOOOO
+            00
+            00
+            00
+            00 MM      MM VVV     VVV  CCCCCC
+            00 MMMM   MMM  VV     VV  CC
+            00 MM MM MM M  VV     VV CCC
+            00 MM  MMM  M   VV   VV  CCC
+            00 MM       M    VV VV    CC
+            00 MM       M     VVV      CCCCCC
+            00            LOLLIPOP-PHP
+            </pre>
+        </article>
+        <link rel="stylesheet" href="<?= View::href('static/css/default.css') ?>">
+        <script type="text/javascript" src="<?= View::href('static/js/jquery-3.2.0.min.js') ?>"></script>
+        <script type="text/javascript" src="<?= View::href('static/js/default.js') ?>"></script>
     </body>
 </html>
