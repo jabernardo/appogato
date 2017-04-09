d88fa4c96d4f6b072dab6ce71cba9b591ad3a3df
John Aldrich Bernardo <4ldrich@protonmail.com>
2017-04-09 20:42:44 +0800

    welcome message: replacing 0

--- lmvc/app/view/hello.php
+++ lmvc/app/view/hello.php
@@ -12,23 +12,23 @@
      O OOOOOOOOOOOOOO
     O OO          OOOO
    O OO OOOOOOOOOOOOOOO
-  0 00 00000      00000
+  O OO OOOOO      OOOOO
   O OO OOOOO OOOO OOOOO
    O OO OOOO  OOOO OOOO
     O OO OOOOOOOO OOOO
      OOOO        OOOO
        OOOOOOOOOOOOO
           OOOOOO
-            00
-            00
-            00
-            00 MM      MM VVV     VVV  CCCCCC
-            00 MMMM   MMM  VV     VV  CC
-            00 MM MM MM M  VV     VV CCC
-            00 MM  MMM  M   VV   VV  CCC
-            00 MM       M    VV VV    CC
-            00 MM       M     VVV      CCCCCC
-            00            LOLLIPOP-PHP
+            OO
+            OO
+            OO
+            OO MM      MM VVV     VVV  CCCCCC
+            OO MMMM   MMM  VV     VV  CC
+            OO MM MM MM M  VV     VV CCC
+            OO MM  MMM  M   VV   VV  CCC
+            OO MM       M    VV VV    CC
+            OO MM       M     VVV      CCCCCC
+            OO            LOLLIPOP-PHP
             </pre>
         </article>
         <link rel="stylesheet" href="<?= View::href('static/css/default.css') ?>">
