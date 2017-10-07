42cd1029ebded335d8f3d654c3c47013700ee9f2
John Aldrich Bernardo <bjohnaldrich@gmail.com>
2017-10-07 15:25:52 +0000

    bin/Cache: code clean-up

--- bin/Cache/Cache.php
+++ bin/Cache/Cache.php
@@ -8,7 +8,7 @@ use \Lollipop\Config;
  * Cache
  * 
  * @package lmvc
- * @version 1.1
+ * @version 1.2
  * @author  John Aldrich Bernardo
  * @email   4ldrich@protonmail.com
  * @description
@@ -19,6 +19,18 @@ use \Lollipop\Config;
 class Cache
 {
     /**
+     * @var     array   Arguments
+     * 
+     */
+    private $_args = [];
+    
+    /**
+     * @var     string  Active command
+     * 
+     */
+    private $_command = '';
+    
+    /**
      * Class Construct
      * 
      * @param   array   Project config
@@ -27,31 +39,46 @@ class Cache
      * 
      */
     function __construct(array $args) {
-        $this->_args = $args;
+        // Get [0] as active command
         $this->_command = isset($args[0]) ? $args[0] : '';
         
+        // Remaining will be treated as parameters
         $this->_args = array_splice($args, 1, count($args) - 1);
         
         switch (strtolower($this->_command)) {
-            case 'purge':       // Cache purging
+            case 'purge':
+                // Cache purging
                 $this->_purge();
+                
                 break;
-            case 'remove':      // Remove cache
+            case 'remove':
+                // Remove cache
                 if (!count($this->_args)) {
+                    // This functionality requires arguments
+                    // as cache key
                     Console::error('Missing keys.');
                 }
                 
+                // Execute `remove`
                 $this->_remove();
+                
                 break;
-            case 'get':      // Remove cache
+            case 'get':
+                // Recover cache
                 if (!count($this->_args)) {
+                    // This functionality requires arguments
+                    // as cache key
                     Console::error('Missing keys.');
                 }
                 
+                // Get cache value
                 $this->_get();
+                
                 break;
             default:
+                // Nah...
                 Console::error('Unknown command.');
+                
                 break;
         }
     }
@@ -64,11 +91,16 @@ class Cache
      * 
      */
     private function _get() {
-        $i = 0;
+        $i = 0; // Key index
+        
         foreach($this->_args as $key) {
+            // Display value for cache key
+            // [{key}]
+            // {value}
             echo "[$key]", PHP_EOL, (string)\Lollipop\Cache::recover($key), PHP_EOL;
             
             if ($i + 1 < count($this->_args))
+                // Some separator for each key...
                 echo '--------------------------------------------------------------', PHP_EOL;
             
             $i++;
@@ -83,6 +115,9 @@ class Cache
      * 
      */
     private function _remove() {
+        // All configuration, regarding cache folder will
+        // be load by `Lollipop\Cache` from what configuration is
+        // set on application `app/config.php`
         foreach($this->_args as $key) {
             \Lollipop\Cache::remove($key);
         }
@@ -96,6 +131,8 @@ class Cache
      * 
      */
     private function _purge() {
+        // Remove all cache from cache folder or database
+        // configured on application `app/config.php`
         \Lollipop\Cache::purge();
     }
 }
