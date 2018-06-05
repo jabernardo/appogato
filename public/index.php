5f6dc7175629bc1071140b55890a5dd6317cc18d
John Aldrich Bernardo <bjohnaldrich@gmail.com>
2018-06-05 09:52:12 +0000

    index: some style

--- public/index.php
+++ public/index.php
@@ -1,16 +1,22 @@
 <?php
 
 /**
- * Lollipop for MVC
- * An extensive and flexible library for PHP
+ * 
+ * -----------------------------------------------------------------------------
+ * Lollipop for MVC - An extensive and flexible library for PHP
+ * -----------------------------------------------------------------------------
  *
  * @package    Lollipop for MVC
- * @version    2.0
+ * @version    3.0
  * @author     John Aldrich Bernardo <bjohnaldrich@gmail.com>
- * @copyright  Copyright (C) 2017 John Aldrich Bernardo. All rights reserved.
+ * @copyright  Copyright (C) 2018 John Aldrich Bernardo. All rights reserved.
  * @license
  *
- * Copyright (c) 2017 John Aldrich Bernardo
+ * -----------------------------------------------------------------------------
+ * MIT License
+ * -----------------------------------------------------------------------------
+ * 
+ * Copyright (c) 2018 John Aldrich Bernardo
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -33,45 +39,76 @@
  *
  */
 
+
 /**
- * Load Lollipop-PHP
- *
+ * -----------------------------------------------------------------------------
+ * Composer Auto Loading
+ * -----------------------------------------------------------------------------
+ * 
+ * LMVC requires the following packages as dependency:
+ * 
+ *  - jabernardo/console.php
+ *      Used in command-line based application in managing LMVC
+ * 
+ *  - jabernardo/lollipop-php
+ *      Main framework used as back-end
  */
 require_once(realpath(dirname(__DIR__)) . '/vendor/autoload.php');
 
-/**
- * Paths
- * 
- */
-define('APP_ROOT', realpath(dirname(__DIR__)));
-define('APP_CORE', APP_ROOT . '/app/');
-define('APP_CORE_CONFIG', APP_CORE . 'config/');
-define('APP_CORE_ROUTES', APP_CORE . 'routes/');
-define('APP_CORE_CONTROLLER', APP_CORE . 'controller/');
-define('APP_CORE_MODEL', APP_CORE . 'model/');
-define('APP_CORE_VIEW', APP_CORE . 'view/');
-define('APP_CORE_HELPER', APP_CORE . 'helper/');
-define('APP_CORE_MIDDLEWARE', APP_CORE . 'middleware/');
-define('APP_CORE_DEBUG', APP_CORE . 'debug/');
-define('APP_CORE_STORAGE', APP_CORE . 'storage/');
-define('APP_CORE_CACHE', APP_CORE_STORAGE . 'cache/');
-define('APP_CORE_LOG', APP_CORE_STORAGE . 'log/');
-define('APP_CORE_DB', APP_CORE_STORAGE . 'db/');
-define('APP_CORE_UPLOAD', APP_CORE_STORAGE . 'upload/');
 
 /**
- * Autoload function
- * for controllers
+ * -----------------------------------------------------------------------------
+ * Declaration of folders within the application
+ * -----------------------------------------------------------------------------
+ * 
+ * Make sure that all folders are existing ***
+ * 
+ */
+define('APP_ROOT',              realpath(dirname(__DIR__)));
+define('APP_CORE',              APP_ROOT . '/app/');
+define('APP_CORE_CONFIG',       APP_CORE . 'config/');
+define('APP_CORE_ROUTES',       APP_CORE . 'routes/');
+define('APP_CORE_CONTROLLER',   APP_CORE . 'controller/');
+define('APP_CORE_MODEL',        APP_CORE . 'model/');
+define('APP_CORE_VIEW',         APP_CORE . 'view/');
+define('APP_CORE_HELPER',       APP_CORE . 'helper/');
+define('APP_CORE_MIDDLEWARE',   APP_CORE . 'middleware/');
+define('APP_CORE_DEBUG',        APP_CORE . 'debug/');
+define('APP_CORE_STORAGE',      APP_CORE . 'storage/');
+define('APP_CORE_CACHE',        APP_CORE_STORAGE . 'cache/');
+define('APP_CORE_LOG',          APP_CORE_STORAGE . 'log/');
+define('APP_CORE_DB',           APP_CORE_STORAGE . 'db/');
+define('APP_CORE_UPLOAD',       APP_CORE_STORAGE . 'upload/');
+
+
+/**
+ * -----------------------------------------------------------------------------
+ * Auto-Loading
+ * -----------------------------------------------------------------------------
+ * 
+ * LMVC requires to auto-load the following namespaces:
+ * 
+ *  - \App\Controller
+ *  - \App\Model
+ *  - \App\Helper
+ *  - \App\Middleware
  * 
  */
 spl_autoload_register(function($class) {
     // project-specific namespace prefix
     $prefixes = array(
-            'App\\Controller\\' => APP_CORE_CONTROLLER,
-            'App\\Model\\' => APP_CORE_MODEL,
-            'App\\Helper\\' => APP_CORE_HELPER,
-            'App\\Middleware\\' => APP_CORE_MIDDLEWARE
-        );
+        // Application Controllers Namespace
+        'App\\Controller\\' => APP_CORE_CONTROLLER,
+        
+        // Application Model Namespace
+        'App\\Model\\' => APP_CORE_MODEL,
+        
+        // Application Helper Namespace
+        'App\\Helper\\' => APP_CORE_HELPER,
+        
+        // Application Middlewares Namespace
+        'App\\Middleware\\' => APP_CORE_MIDDLEWARE
+    );
 
     foreach ($prefixes as $prefix => $base_dir) {
         // does the class use the namespace prefix?
@@ -97,22 +134,41 @@ spl_autoload_register(function($class) {
     }
 });
 
+
 /**
- * Load configuration files
- *
+ * -----------------------------------------------------------------------------
+ * Lollipop-PHP for MVC: Configuration
+ * -----------------------------------------------------------------------------
+ * 
+ * Configuration in initializing lollipop application
+ * 
+ * @var     $config     array
+ * 
  */
 $config = [];
 
+/**
+ * Load all configuration files in configurations folder
+ * 
+ */
 foreach (glob(APP_CORE_CONFIG . '*.php') as $config_file) {
     $config = array_merge_recursive($config, include($config_file));
 }
 
+/**
+ * Destroy application if set with wrong configurations
+ *
+ */
 if (!isset($config) && !is_array($config))
     die('Configuration was invalid');
     
 /**
  * Check if environment is valid
- * 
+ *
+ * Valid environments are:
+ *  - dev (development)
+ *  - stg (staging)
+ *  - prd (production)
  */
 if (isset($config['env'])) {
     switch (strtolower($config['env'])) {
@@ -127,13 +183,18 @@ if (isset($config['env'])) {
 }
 
 /**
- * Initialize configuration in Lollipop
+ * Initialize configuration in Lollipop-PHP
  * 
  */
 \Lollipop\Config::load($config);
 
+
 /**
- * Include routes
+ * -----------------------------------------------------------------------------
+ * Application Routes
+ * -----------------------------------------------------------------------------
+ * 
+ * Load all routes in routes folder
  * 
  */
 foreach (glob(APP_CORE_ROUTES . '*.php') as $route_file) {
@@ -144,32 +205,55 @@ if (!isset($route) && !is_array($route))
     die('Invalid routes!');
 
 
+/**
+ * -----------------------------------------------------------------------------
+ * SAPI checking
+ * -----------------------------------------------------------------------------
+ * 
+ * LMVC was designed for the web. Also it supports command-line applications
+ * for managing its own
+ * 
+ * `return` is expected for LMVC command-line application to load configurations
+ * and routes
+ * 
+ */
 if (!strcmp(php_sapi_name(), 'cli'))
     return;
 
+
 /**
- * Lollipop Debugger
+ * -----------------------------------------------------------------------------
+ * LMVC Debug
+ * -----------------------------------------------------------------------------
+ * 
+ * See `app/debug` folder
  * 
  */
 require_once(APP_CORE_DEBUG . 'info.php');
 
+
 /**
- * Controller Pre-fix for LMVC Application
- *
+ * -----------------------------------------------------------------------------
+ * Dispatcher
+ * -----------------------------------------------------------------------------
+ * 
  */
 $controller_prefix = 'App\\Controller\\';
 
 /**
- * Register every route
- * to lollipop dispatcher
+ * Register every route for serving
  * 
  */
 foreach ($route as $key => $value) {
     if (is_callable($value)) {
+        // Callable routes
         \Lollipop\HTTP\Router::all($key, $value);
     } else if (is_string($value)) {
+        // Basic Routes using controllers are allowed to be access
+        // using any HTTP methods
         \Lollipop\HTTP\Router::all($key, $controller_prefix . $value);
     } else if (is_array($value)) {
+        // Advance routing for using middlewares or strict HTTP method checking
         $value['path'] = $key;
         
         if (is_string($value['callback'])) {
