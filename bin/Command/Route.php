9b40ffe794c82b17b47645dbaf0e81a0695dc895
John Aldrich Bernardo <bjohnaldrich@gmail.com>
2018-07-07 06:20:36 +0000

    bin/command/route: some cs fix

--- bin/Command/Route.php
+++ bin/Command/Route.php
@@ -24,14 +24,17 @@ class Route implements \Console\Command {
      */
     private function _help(\Console\Input $i, \Console\Output $o) {
         $o->writeln('Appogato Route Tester' . PHP_EOL);
-        // Save cache
+        
+        // Test Route
         $o->writeln('-- Parameters --');
-        $o->writeln(str_pad('uri', 15) . 'Uri Path');
-        $o->writeln(str_pad('method', 15) . 'Request method');
-        $o->writeln(str_pad('query', 15) . 'Request quries');
-        $o->writeln(str_pad('data', 15) . 'Request data');
+        $o->writeln(str_pad('uri', 15)      . 'Uri Path');
+        $o->writeln(str_pad('method', 15)   . 'Request method');
+        $o->writeln(str_pad('query', 15)    . 'Request quries');
+        $o->writeln(str_pad('data', 15)     . 'Request data');
+        
         $o->writeln('-- Example --');
         $o->writeln('./appogato route uri:/ query:"disable-debugger&message=test"');
+        
         // --------
         $o->write(PHP_EOL);
     }
@@ -47,8 +50,10 @@ class Route implements \Console\Command {
         $one_param = count($i->getParameters()) === 1;
         
         if (in_array('help', $i->getParameters()) && $one_param) {
+            // Help Screen
             $this->_help($i, $o);
         } else {
+            // Get parameters from arguments
             $uri = Utils::spare($i->getOption('uri'), '/');
             $method = Utils::spare($i->getOption('method'), 'GET');
             $query = Utils::spare($i->getOption('query'), '');
@@ -59,6 +64,8 @@ class Route implements \Console\Command {
                 $decoded = [];
             }
             
+            // Declare super globals in-order to emulate
+            // web sapi
             $_SERVER = [];
             $_SERVER['REQUEST_URI'] = $uri;
             $_SERVER['SERVER_NAME'] = 'localhost';
@@ -73,7 +80,7 @@ class Route implements \Console\Command {
             parse_str($query, $queries);
             $_GET = $queries;
             
-            
+            // Dispatch route
             $res = \Lollipop\HTTP\Router::dispatch(false);
             
             if (!($res instanceof \Lollipop\HTTP\Response))  {
